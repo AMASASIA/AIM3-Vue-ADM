4a6a9832c2cf99669d1fe20e4d3c9499275cb47a
AMASASIA <avantamas@gmail.com>
2025-09-08 19:01:12 +0900

    0909_ index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,2 +1,17 @@
-import { createRouter, createWebHistory } from 'vue-router';
-export default createRouter({ history: createWebHistory(), routes: [] });
+import { createRouter, createWebHistory } from 'vue-router'
+import ChatMessaging from '../components/ChatMessaging.vue'
+import VideoChat from '../components/VideoChat.vue'
+import AiMap from '../components/AiMap.vue'
+
+const routes = [
+  { path: '/', component: ChatMessaging },
+  { path: '/video', component: VideoChat },
+  { path: '/map', component: AiMap },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+export default router
